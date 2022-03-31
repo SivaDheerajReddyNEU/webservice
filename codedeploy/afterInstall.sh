fc3ebebcc0b28449e551fcde0d8fc4fb3391a799
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-03-31 11:24:31 -0400

    code changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 sudo systemctl stop application_boot.service
+
+mkdir 
+mv -r ./* /tmp/webserver/
+cd
+mv -r /tmp/webserver webserver
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sudo systemctl stop application_boot.service
-rm -r ./source/ package-lock.json package.json startup.sh README.md node_startup.sh manifest.json aws-linux-ami.pkr.hcl appspec.yml application_boot.service test/ node_modules
+
+cd
+rm -rf webserver
