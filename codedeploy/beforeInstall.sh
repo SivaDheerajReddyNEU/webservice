d2653200a62fcb78b1920f827bf946effb7d3a01
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-03-31 13:45:31 -0400

    code changes

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sudo systemctl stop application_boot.service
-pwd 
-echo " in home"
+echo "start"
 pwd 
 ls
 echo "removing old webserver"
 rm -rf /tmp/webserver
 echo "Creating  webserver folder in temp"
 mkdir /tmp/webserver
+echo "end"
