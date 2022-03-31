ede724bb2c0a909d0c8a927bbf2b70fe55057eba
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-03-31 09:54:58 -0400

    code changes

--- startup.sh
+++ startup.sh
@@ -19,7 +19,7 @@ $CODEDEPLOY_BIN stop
 yum erase codedeploy-agent -y
 
 #installing code deploy
-sudo yum install ruby
+sudo yum install ruby -y
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
