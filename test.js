88f3ff45414ac189f46105595d6023e98032864d
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-04-07 18:24:23 -0400

    code watch changes added

--- test.js
+++ test.js
@@ -1,19 +1,8 @@
-var Client = require('node-statsd-client').Client;
+var StatsD = require('hot-shots'),
+client = new StatsD({
+    port: 8125,
+    host:localhost
+});
 
-var client = new Client("localhost", 8125);
-
-// Count stat
-client.count("num_logged_users", 1);
-client.increment("num_logged_users");
-client.decrement("num_logged_users");
-
-// Timing stat
-client.timing("request_ms", 250);
-
-// Gauge stat
-client.gauge("gauge_stats", 4);
-
-// Set stat
-client.set("set_stats", 3);
-
-module.exports=client;
+// Increment: Increments a stat by a value (default is 1)
+client.increment('my_counter');
