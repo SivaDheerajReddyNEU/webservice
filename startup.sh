6be17ff2afe8d52eb2fb6ec9a03bde9075bba0f4
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-04-07 22:49:38 -0400

    git workflow changes

--- startup.sh
+++ startup.sh
@@ -49,6 +49,7 @@ sudo mv /tmp/application_boot.service /etc/systemd/system/application_boot.servi
 print_command_info CHANGING-PERMISSIONS-OF-APPLICATION-BOOT-FILE
 sudo chmod 777 /etc/systemd/system/application_boot.service
 print_command_info ENABLING-BOOT-UP-FOR-APP
+
 sudo systemctl enable application_boot.service
 sudo systemctl enable amazon-cloudwatch-agent.service
 
